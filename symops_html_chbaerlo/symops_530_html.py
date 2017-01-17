8b17c5613152d1af3f0a24c4ea4492521cee020a
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2017-01-17 10:20:00 -0800

    generate csv

--- symops_html_chbaerlo/symops_530_html.py
+++ symops_html_chbaerlo/symops_530_html.py
@@ -4,13 +4,18 @@ from cctbx import sgtbx
 import symops_530_html_markup_cb
 
 
+def html_markup(symbol):
+  return [symops_530_html_markup_cb.html_markup[plain]
+          for plain in symbol.split()]
+
+
 def run():
   for symbols in sgtbx.space_group_symbol_iterator():
-    symbol = symbols.universal_hermann_mauguin()
-    html = [
-        symops_530_html_markup_cb.html_markup[plain]
-        for plain in symbol.split()]
-    print ' '.join(html), ' #', symbols.number()
+    hm = html_markup(symbols.hermann_mauguin())
+    ext = symbols.extension()
+    if ext == '\0': ext = ''
+    uhm = html_markup(symbols.universal_hermann_mauguin())
+    print '%s,%s,%s,%s' % (symbols.number(), ''.join(hm), ext, ' '.join(uhm))
 
 
 if __name__ == '__main__':
