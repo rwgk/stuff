0d404f023d4bbf7431598709a36116e149ae5b9f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-05-07 20:27:45 -0700

    add leading ./ in ./a.out

--- caster_odr/compile_and_run.sh
+++ caster_odr/compile_and_run.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
-clang++ -std=c++11 car.cc truck.cc vehicle_specs.cc && a.out
-clang++ -std=c++11 truck.cc car.cc vehicle_specs.cc && a.out
-clang++ -std=c++11 -DUSE_TRANSLATION_UNIT_TAG car.cc truck.cc vehicle_specs.cc && a.out
-clang++ -std=c++11 -DUSE_TRANSLATION_UNIT_TAG truck.cc car.cc vehicle_specs.cc && a.out
+clang++ -std=c++11 car.cc truck.cc vehicle_specs.cc && ./a.out
+clang++ -std=c++11 truck.cc car.cc vehicle_specs.cc && ./a.out
+clang++ -std=c++11 -DUSE_TRANSLATION_UNIT_TAG car.cc truck.cc vehicle_specs.cc && ./a.out
+clang++ -std=c++11 -DUSE_TRANSLATION_UNIT_TAG truck.cc car.cc vehicle_specs.cc && ./a.out
 rm a.out
