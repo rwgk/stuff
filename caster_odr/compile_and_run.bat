da2d61a73ab3ccbf265c95420ca14cf46a90b3f3
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2022-05-08 06:21:05 -0700

    Add compile_and_run.bat for Windows.

---
+++ caster_odr/compile_and_run.bat
@@ -0,0 +1,8 @@
+cl car.cc truck.cc vehicle_specs.cc /Fe"show.exe"
+.\show
+cl truck.cc car.cc vehicle_specs.cc /Fe"show.exe"
+.\show
+cl /DUSE_TRANSLATION_UNIT_TAG car.cc truck.cc vehicle_specs.cc /Fe"show.exe"
+.\show
+cl /DUSE_TRANSLATION_UNIT_TAG truck.cc car.cc vehicle_specs.cc /Fe"show.exe"
+.\show
