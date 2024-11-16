7d93794069898f245e14fb94d779816291fde1ff
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-11-16 11:04:52 -0800

    Fix weakref_finalize_toy_example.py
    Before this change, `self.close()` is invoked only at interpreter shutdown.
    
    With this change, `self._members.close()` is invoked as soon as a `ShopKeeper`
    object goes out of scope.
    
    Quoting from
    
    https://docs.python.org/3/library/weakref.html#weakref.finalize
    
    > Note It is important to ensure that func, args and kwargs do not own any
    > references to obj, either directly or indirectly, since otherwise obj will
    > never be garbage collected. In particular, func should not be a bound method
    > of obj.
    
    Side note: Do NOT trust current Gemini Generative AI (in Google search).

--- random_attic/weakref_finalize_toy_example.py
+++ random_attic/weakref_finalize_toy_example.py
@@ -1,22 +1,44 @@
 import weakref
 
+
 class ShopKeeper:
-    __slots__ = ("__weakref__", "serno", "exception_harness")
+    class _Members:
+        __slots__ = ("serno", "exception_harness")
+
+        def __init__(self, serno, exception_harness):
+            self.serno = serno
+            self.exception_harness = exception_harness
+
+        def close(self):
+            if self.exception_harness:
+                try:
+                    print("        Close", self.serno, 1 / (self.serno - 1))
+                except Exception:
+                    print("        PROBLEM", self.serno)
+            else:
+                print("        Close", self.serno, 1 / (self.serno - 1))
+
+    __slots__ = ("__weakref__", "_members")
 
     def __init__(self, serno, exception_harness):
-        self.serno = serno
-        self.exception_harness = exception_harness
-        weakref.finalize(self, self.close)
+        self._members = ShopKeeper._Members(serno, exception_harness)
+        weakref.finalize(self, self._members.close)
 
-    def close(self):
-        if self.exception_harness:
-            try:
-                print("close", self.serno, 1 / (self.serno - 2))
-            except Exception:
-                print("problem", self.serno)
-        else:
-                print("close", self.serno, 1 / (self.serno - 2))
 
-for exception_harness in [False, True]:
-    for serno in range(5):
-        ShopKeeper(serno, exception_harness)
+def short_term_shop_keeper(serno, exception_harness):
+    print("    Start short term", serno, exception_harness)
+    ShopKeeper(serno, exception_harness)
+    print("    End short term")
+
+
+def experiment():
+    print("Start experiment")
+    for exception_harness in [False, True]:
+        for serno in range(3):
+            short_term_shop_keeper(serno, exception_harness)
+    print("End experiment")
+
+
+if __name__ == "__main__":
+    experiment()
+    print("All done.")
