3275534fc8b363b8486b1d1fa752d9e2c710b5f2
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-10-15 13:47:38 -0700

    Make hello_cuda_world/build_and_run.sh work on native Ubuntu 24.04 again

--- hello_cuda_world/build_and_run.sh
+++ hello_cuda_world/build_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 set -x
-"${CC}" hello_cuda_world.c "-I${CUDA_HOME}/include" "$(ldconfig -p | grep 'libcuda\.so' | sed 's/.* => //')"
+"${CC}" hello_cuda_world.c "-I${CUDA_HOME}/include" "$(ldconfig -p | grep 'libcuda\.so' | sed 's/.* => //' | head -n 1)"
 ./a.out
